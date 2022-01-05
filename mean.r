9ad04b4b03cc7cf2721d314b3826e621d4e1d70a
arpanravinuthala <95072328+arpanravinuthala@users.noreply.github.com>
2022-01-05 08:43:20 +0530

    Create mean.r

---
+++ mean.r
@@ -0,0 +1,17 @@
+print("enter the elements")
+x<-scan()
+sum=0
+for(i in x)
+{
+  sum=sum+i
+}
+l=length(x)
+cat("mean of the given data:",sum/l)print("enter the elements")
+x<-scan()
+sum=0
+for(i in x)
+{
+  sum=sum+i
+}
+l=length(x)
+cat("mean of the given data:",sum/l)
